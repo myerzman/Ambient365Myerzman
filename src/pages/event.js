cf611ff1b27e5dd73464f40bd51c64068ca8a76e
Matt Myers <myerzman@gmail.com>
2023-04-13 10:08:49 -0700

    webinars in events

--- src/pages/event.js
+++ src/pages/event.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "../components/Layout";
 import { graphql, Link } from "gatsby";
-import { GatsbySeo } from 'gatsby-plugin-next-seo';
+import { GatsbySeo } from "gatsby-plugin-next-seo";
 
 import "./event.css";
 
@@ -31,39 +31,50 @@ const event = ({ data }) => {
 	return (
 		<Layout pageName="Events">
 			<GatsbySeo
-      openGraph={{
-        type: 'website',
-        url: 'https://www.ambient365.us/events',
-        title: 'Ambient365 Event',
-        description: 'Stay informed about upcoming events and webinars related to Microsoft365 by visiting our events page. Ambient365 offers a variety of educational and networking opportunities to help you stay ahead of the curve.',
-	  }}
-    />
+				openGraph={{
+					type: "website",
+					url: "https://www.ambient365.us/events",
+					title: "Ambient365 Event",
+					description:
+						"Stay informed about upcoming events and webinars related to Microsoft365 by visiting our events page. Ambient365 offers a variety of educational and networking opportunities to help you stay ahead of the curve.",
+				}}
+			/>
 			<>
 				<h2 className="grayheading">Our Events</h2>
 				<div className="events">
 					{" "}
-					{data.allWpPost.edges.filter(post => post.node.categories.nodes[0].name === "Webinar").reverse().map(({ node }) => {
-						return (
-							<div className="eventWrapper" key={node.slug}>
-								<h2>{node.title}</h2>
-								
-								<div className="date">{node.customDate}</div>
-								<div className="categories">
-									{node.categories.nodes.map(cat => 
-										<div className="cat-item" key={cat.id}>{cat.name}</div>
-								)} </div> 
-								<div
-									dangerouslySetInnerHTML={{
-										__html: node.excerpt,
-									}}
-								/>
-						
-								<Link to={`../${node.slug}`}>
-									<button>Read more</button>
-								</Link>
-							</div>
-						);
-					})}
+					{data.allWpPost.edges
+						.filter(
+							(post) =>
+								post.node.categories.nodes[0].name === "Webinar" ||
+								post.node.categories.nodes[1].name === "Webinar"
+						)
+						.reverse()
+						.map(({ node }) => {
+							return (
+								<div className="eventWrapper" key={node.slug}>
+									<h2>{node.title}</h2>
+
+									<div className="date">{node.customDate}</div>
+									<div className="categories">
+										{node.categories.nodes.map((cat) => (
+											<div className="cat-item" key={cat.id}>
+												{cat.name}
+											</div>
+										))}{" "}
+									</div>
+									<div
+										dangerouslySetInnerHTML={{
+											__html: node.excerpt,
+										}}
+									/>
+
+									<Link to={`../${node.slug}`}>
+										<button>Read more</button>
+									</Link>
+								</div>
+							);
+						})}
 				</div>
 			</>
 		</Layout>
