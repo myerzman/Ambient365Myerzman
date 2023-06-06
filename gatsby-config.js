15b426a22442d774f9db828b185623d152e7fc8c
Matt Myers <myerzman@gmail.com>
2023-06-06 14:32:51 -0700

    adding google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,6 +53,23 @@ module.exports = {
 			},
 		},
 		{
+			resolve: "gatsby-plugin-google-tagmanager",
+			options: {
+				id: "GTM-KJFPNVL",
+
+				// Include GTM in development.
+				//
+				// Defaults to false meaning GTM will only be loaded in production.
+				includeInDevelopment: false,
+
+				// datalayer to be set before GTM is loaded
+				// should be an object or a function that is executed in the browser
+				//
+				// Defaults to null
+				defaultDataLayer: { platform: "gatsby" },
+			},
+		},
+		{
 			resolve: `gatsby-plugin-styled-components`,
 			options: {
 				// Add any options here
